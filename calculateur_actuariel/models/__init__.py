80f6f7cb7275f681436ad88c1ef4159575e51b47
Guy9985 <guyorestendi@gmail.com>
2025-11-07 17:59:33 +0200

    Nettoyage: garder seulement Fianancial_Simulator

--- calculateur_actuariel/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Fichier d'initialisation du package models
-# Permet de traiter le dossier models comme un package Python
-
-from .premium_calculator import PremiumCalculator
-from .database import Database
-
-__all__ = ['PremiumCalculator', 'Database']
